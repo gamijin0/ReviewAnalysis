39666980d1d0a997ee900be431723a43470ece3b
chaos <497386856@qq.com>
2017-03-06 19:07:01 +0800

    remove pip; recorrect using pymysql;

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,2 @@
 FROM python:3-onbuild
-CMD ["pip","install","-r","./requirements.txt"]
 CMD ["python","./run.py"]
--- Module/__init__.py
+++ Module/__init__.py
@@ -1,7 +1,7 @@
 import sqlalchemy as SA
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-engin  = SA.create_engine("mysql+mysqlconnector://root:xlsd1996@chaos.ac.cn/iTunes",encoding='utf-8',echo=False)
+engin  = SA.create_engine("mysql+pymysql://root:xlsd1996@chaos.ac.cn/iTunes",encoding='utf-8',echo=False)
 Session = sessionmaker(autoflush=True)
 Base = declarative_base()
 
